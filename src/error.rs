e9c61d9f90c7f4ade83fc387f2b8d1b3b19d040f
JLer <jlerxky@live.com>
2023-03-24 15:48:07 +0800

    WIP

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,12 @@
+use crdts::{map::CmRDTValidation, CmRDT, Orswot, VClock};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum CrdtError {
     #[error("unknown error")]
     Unknown,
+    #[error("VClock error")]
+    VClock(<VClock<u64> as CmRDT>::Validation),
+    #[error("Map error")]
+    Map(CmRDTValidation<Orswot<Vec<u8>, u64>, u64>),
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod error;
 
-use crdts::{CmRDT, CvRDT, List, Map, Orswot, VClock};
-use error::CrdtError;
+use core::fmt::Debug;
 
-#[derive(Clone, Debug, PartialEq, Default)]
+use crdts::{CmRDT, CvRDT, Dot, Map, Orswot, VClock};
+use error::CrdtError;
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Controller {
     clock: VClock<u64>,
-    txns: Orswot<&'static str, u64>,
+    txns: Orswot<String, u64>,
     history_hashes: Map<u64, Orswot<Vec<u8>, u64>, u64>,
     candidates: Orswot<Vec<u8>, u64>,
 }
@@ -14,35 +17,114 @@ pub struct Controller {
 impl Controller {
     pub fn new() -> Self {
         Self {
+            clock: Default::default(),
             txns: Orswot::new(),
             history_hashes: Map::new(),
             candidates: Orswot::new(),
-            clock: Default::default(),
         }
     }
 }
 
+pub struct Op {
+    dot: Dot<u64>,
+    op: OpOneByOne,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum OpOneByOne {
+    T(crdts::orswot::Op<String, u64>),
+    H(crdts::map::Op<u64, Orswot<Vec<u8>, u64>, u64>),
+    C(crdts::orswot::Op<Vec<u8>, u64>),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum OpAllInOne {
+    All(
+        Option<crdts::orswot::Op<String, u64>>,
+        Option<crdts::map::Op<u64, Orswot<Vec<u8>, u64>, u64>>,
+        Option<crdts::orswot::Op<Vec<u8>, u64>>,
+    ),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum OpMix {
+    TaHuCa,
+    TrHuCa,
+    TaHrCa,
+    TaHuCr,
+    // ...
+}
+
 impl CmRDT for Controller {
-    type Op = String;
+    type Op = Op;
     type Validation = CrdtError;
 
     fn apply(&mut self, op: Self::Op) {
-        todo!()
+        match op.op {
+            OpOneByOne::T(o) => {
+                if self.clock.get(&op.dot.actor) >= op.dot.counter {
+                    return;
+                }
+                self.txns.apply(o);
+
+                self.clock.apply(op.dot);
+            }
+            OpOneByOne::H(o) => {
+                if self.clock.get(&op.dot.actor) >= op.dot.counter {
+                    return;
+                }
+                self.history_hashes.apply(o);
+
+                self.clock.apply(op.dot);
+            }
+            OpOneByOne::C(o) => {
+                if self.clock.get(&op.dot.actor) >= op.dot.counter {
+                    return;
+                }
+                self.candidates.apply(o);
+
+                self.clock.apply(op.dot);
+            }
+        }
     }
 
     fn validate_op(&self, op: &Self::Op) -> Result<(), Self::Validation> {
-        todo!()
+        match &op.op {
+            OpOneByOne::T(op) => match &op {
+                crdts::orswot::Op::Add { dot, .. } => {
+                    self.clock.validate_op(dot).map_err(CrdtError::VClock)?;
+                    self.txns.validate_op(op).map_err(CrdtError::VClock)
+                }
+                crdts::orswot::Op::Rm { .. } => Ok(()),
+            },
+            OpOneByOne::H(op) => {
+                match &op {
+                    crdts::map::Op::Rm { .. } => {}
+                    crdts::map::Op::Up { dot, .. } => {
+                        self.clock.validate_op(dot).map_err(CrdtError::VClock)?;
+                    }
+                }
+                self.history_hashes.validate_op(op).map_err(CrdtError::Map)
+            }
+            OpOneByOne::C(op) => match &op {
+                crdts::orswot::Op::Add { dot, .. } => {
+                    self.clock.validate_op(dot).map_err(CrdtError::VClock)?;
+                    self.candidates.validate_op(op).map_err(CrdtError::VClock)
+                }
+                crdts::orswot::Op::Rm { .. } => Ok(()),
+            },
+        }
     }
 }
 
 impl CvRDT for Controller {
     type Validation = CrdtError;
 
-    fn validate_merge(&self, other: &Self) -> Result<(), Self::Validation> {
+    fn validate_merge(&self, _other: &Self) -> Result<(), Self::Validation> {
         todo!()
     }
 
-    fn merge(&mut self, other: Self) {
+    fn merge(&mut self, _other: Self) {
         todo!()
     }
 }
@@ -51,13 +133,33 @@ fn main() {}
 
 #[test]
 fn test() {
-    use crdts::{CmRDT, Orswot};
-    let mut set: Orswot<u8, &'static str> = Default::default();
-    let add_ctx = set.read_ctx().derive_add_ctx("actor");
-    set.apply(set.add(50, add_ctx));
-    let add_ctx = set.read_ctx().derive_add_ctx("actor1");
-    set.apply(set.add(50, add_ctx));
-    let mut items: Vec<_> = set.iter().map(|item_ctx| *item_ctx.val).collect();
-    items.sort();
-    assert_eq!(items, &[50]);
+    let mut controller = Controller::new();
+    let op1 = controller.txns.add(
+        "member".to_owned(),
+        controller.txns.read().derive_add_ctx(9_742_820),
+    );
+    controller.apply(Op {
+        dot: Dot::new(9_742_820, 2),
+        op: OpOneByOne::T(op1),
+    });
+    let op2 = controller.candidates.add(
+        vec![8; 20],
+        controller.txns.read().derive_add_ctx(9_742_820),
+    );
+    controller.apply(Op {
+        dot: Dot::new(9_742_820, 3),
+        op: OpOneByOne::C(op2),
+    });
+    let add_ctx = controller
+        .history_hashes
+        .read_ctx()
+        .derive_add_ctx(9_742_820);
+    let op3 = controller
+        .history_hashes
+        .update(10u64, add_ctx, |v, a| v.add(vec![8; 20], a));
+    controller.apply(Op {
+        dot: Dot::new(9_742_820, 4),
+        op: OpOneByOne::H(op3),
+    });
+    println!("{:#?}", controller);
 }
