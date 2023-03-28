76808ab5ef870f275a231e62f0ad41376f04d5d5
JLer <jlerxky@live.com>
2023-03-28 16:35:22 +0800

    add actor_type

--- src/proc_macro_one.rs
+++ src/proc_macro_one.rs
@@ -1,10 +1,10 @@
 use crdts::{CmRDT, GCounter, Map, Orswot};
 use crdts_derive::{crdt, CRDT};
 
-#[crdt]
+#[crdt(u64)]
 #[derive(Default, Debug)]
 pub struct ControllerOne {
-    txns: Orswot<String, u64>,
+    txns: Orswot<String, String>,
     history_hashes: Map<u64, Orswot<Vec<u8>, u64>, u64>,
     candidates: Orswot<Vec<u8>, u64>,
     block_height: GCounter<u64>,
@@ -13,53 +13,33 @@ pub struct ControllerOne {
 #[test]
 fn test() {
     let mut controller = ControllerOne::default();
-    let actor = 9_742_820;
-    let counter = 1;
-    let dot = crdts::Dot::new(actor, counter);
-    let op1 = controller.txns.add(
-        "member".to_owned(),
-        controller.txns.read().derive_add_ctx(actor),
-    );
+    for actor in 0..10 {
+        for counter in 0..3 {
+            let dot = crdts::Dot::new(actor, counter);
+            let op1 = controller.txns.add(
+                format!("{actor}-{counter}"),
+                controller.txns.read().derive_add_ctx(actor.to_string()),
+            );
 
-    let add_ctx = controller.history_hashes.read_ctx().derive_add_ctx(actor);
-    let op2 = controller
-        .history_hashes
-        .update(10u64, add_ctx, |v, a| v.add(vec![8; 20], a));
+            let add_ctx = controller.history_hashes.read_ctx().derive_add_ctx(actor);
+            let op2 = controller
+                .history_hashes
+                .update(actor, add_ctx, |v, a| v.add(vec![actor as u8; 20], a));
 
-    let op3 = controller
-        .candidates
-        .add(vec![8; 20], controller.txns.read().derive_add_ctx(actor));
-    controller.apply(ControllerOneCrdtOp {
-        dot,
-        txns_op: Some(op1),
-        history_hashes_op: Some(op2),
-        candidates_op: Some(op3),
-        block_height_op: None,
-    });
-    println!("{:#?}", controller);
+            let op3 = controller.candidates.add(
+                vec![actor as u8; 20],
+                controller.candidates.read().derive_add_ctx(actor),
+            );
 
-    let actor = 9_742_820;
-    let counter = 2;
-    let dot = crdts::Dot::new(actor, counter);
-    let op1 = controller.txns.add(
-        "member".to_owned(),
-        controller.txns.read().derive_add_ctx(actor),
-    );
-
-    let add_ctx = controller.history_hashes.read_ctx().derive_add_ctx(actor);
-    let op2 = controller
-        .history_hashes
-        .update(10u64, add_ctx, |v, a| v.add(vec![8; 20], a));
-
-    let op3 = controller
-        .candidates
-        .add(vec![8; 20], controller.txns.read().derive_add_ctx(actor));
-    controller.apply(ControllerOneCrdtOp {
-        dot,
-        txns_op: Some(op1),
-        history_hashes_op: Some(op2),
-        candidates_op: Some(op3),
-        block_height_op: None,
-    });
-    println!("{:#?}", controller);
+            let op4 = controller.block_height.inc(actor);
+            controller.apply(ControllerOneCrdtOp {
+                dot,
+                txns_op: Some(op1),
+                history_hashes_op: Some(op2),
+                candidates_op: Some(op3),
+                block_height_op: Some(op4),
+            });
+            println!("{:#?}", controller);
+        }
+    }
 }
