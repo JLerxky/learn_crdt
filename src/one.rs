667543a24bf5f39c8dbe452895de3320d31755a6
JLer <jlerxky@live.com>
2023-03-29 15:27:44 +0800

    impl CvRDT

--- src/one.rs
+++ src/one.rs
@@ -11,7 +11,7 @@ pub struct Controller {
 }
 
 #[derive(Debug, PartialEq, Eq)]
-pub enum ControllerError {
+pub enum ControllerCmRDTError {
     NoneOp,
     VClock(<VClock<u64> as CmRDT>::Validation),
     Txns(<Orswot<String, u64> as CmRDT>::Validation),
@@ -19,13 +19,29 @@ pub enum ControllerError {
     Candidates(<Orswot<Vec<u8>, u64> as CmRDT>::Validation),
 }
 
-impl std::fmt::Display for ControllerError {
+impl std::fmt::Display for ControllerCmRDTError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         Debug::fmt(&self, f)
     }
 }
 
-impl std::error::Error for ControllerError {}
+impl std::error::Error for ControllerCmRDTError {}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum ControllerCvRDTError {
+    VClock(<VClock<u64> as CvRDT>::Validation),
+    Txns(<Orswot<String, u64> as CvRDT>::Validation),
+    HistoryHashes(<Map<u64, Orswot<Vec<u8>, u64>, u64> as CvRDT>::Validation),
+    Candidates(<Orswot<Vec<u8>, u64> as CvRDT>::Validation),
+}
+
+impl std::fmt::Display for ControllerCvRDTError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Debug::fmt(&self, f)
+    }
+}
+
+impl std::error::Error for ControllerCvRDTError {}
 
 #[allow(clippy::type_complexity)]
 pub struct Op {
@@ -37,7 +53,7 @@ pub struct Op {
 
 impl CmRDT for Controller {
     type Op = Op;
-    type Validation = ControllerError;
+    type Validation = ControllerCmRDTError;
 
     fn apply(&mut self, op: Self::Op) {
         let Op {
@@ -75,22 +91,24 @@ impl CmRDT for Controller {
         } = op;
         self.v_clock
             .validate_op(dot)
-            .map_err(ControllerError::VClock)?;
+            .map_err(ControllerCmRDTError::VClock)?;
         match &(txns_op, history_hashes_op, candidates_op) {
-            (None, None, None) => Err(ControllerError::NoneOp),
+            (None, None, None) => Err(ControllerCmRDTError::NoneOp),
             (t_op, h_op, c_op) => {
                 if let Some(t_op) = t_op {
-                    self.txns.validate_op(t_op).map_err(ControllerError::Txns)?;
+                    self.txns
+                        .validate_op(t_op)
+                        .map_err(ControllerCmRDTError::Txns)?;
                 }
                 if let Some(h_op) = h_op {
                     self.history_hashes
                         .validate_op(h_op)
-                        .map_err(ControllerError::HistoryHashes)?;
+                        .map_err(ControllerCmRDTError::HistoryHashes)?;
                 }
                 if let Some(c_op) = c_op {
                     self.candidates
                         .validate_op(c_op)
-                        .map_err(ControllerError::Candidates)?;
+                        .map_err(ControllerCmRDTError::Candidates)?;
                 }
                 Ok(())
             }
@@ -99,19 +117,34 @@ impl CmRDT for Controller {
 }
 
 impl CvRDT for Controller {
-    type Validation = ControllerError;
+    type Validation = ControllerCvRDTError;
 
-    fn validate_merge(&self, _other: &Self) -> Result<(), Self::Validation> {
-        todo!()
+    fn validate_merge(&self, other: &Self) -> Result<(), Self::Validation> {
+        self.v_clock
+            .validate_merge(&other.v_clock)
+            .map_err(Self::Validation::VClock)?;
+        self.txns
+            .validate_merge(&other.txns)
+            .map_err(Self::Validation::Txns)?;
+        self.history_hashes
+            .validate_merge(&other.history_hashes)
+            .map_err(Self::Validation::HistoryHashes)?;
+        self.candidates
+            .validate_merge(&other.candidates)
+            .map_err(Self::Validation::Candidates)?;
+        Ok(())
     }
 
-    fn merge(&mut self, _other: Self) {
-        todo!()
+    fn merge(&mut self, other: Self) {
+        self.v_clock.merge(other.v_clock);
+        self.txns.merge(other.txns);
+        self.history_hashes.merge(other.history_hashes);
+        self.candidates.merge(other.candidates);
     }
 }
 
 #[test]
-fn test() {
+fn test_cmrdt() {
     let mut controller = Controller::default();
     let actor = 9_742_820;
     let counter = 1;
@@ -160,4 +193,60 @@ fn test() {
         candidates_op: Some(op3),
     });
     println!("{:#?}", controller);
+}
+
+#[test]
+fn test_cvrdt() {
+    let mut controller1 = Controller::default();
+    let mut controller2 = controller1.clone();
+    let actor = 9_742_820;
+    let counter = 1;
+    let dot = Dot::new(actor, counter);
+    let op1 = controller1.txns.add(
+        "member".to_owned(),
+        controller1.txns.read().derive_add_ctx(actor),
+    );
+
+    let add_ctx = controller1.history_hashes.read_ctx().derive_add_ctx(actor);
+    let op2 = controller1
+        .history_hashes
+        .update(10u64, add_ctx, |v, a| v.add(vec![8; 20], a));
+
+    let op3 = controller1
+        .candidates
+        .add(vec![8; 20], controller1.txns.read().derive_add_ctx(actor));
+    controller1.apply(Op {
+        dot,
+        txns_op: Some(op1),
+        history_hashes_op: Some(op2),
+        candidates_op: Some(op3),
+    });
+    println!("controller1: {:#?}", controller1);
+
+    let actor = 9_742_821;
+    let counter = 1;
+    let dot = Dot::new(actor, counter);
+    let op1 = controller2.txns.add(
+        "member".to_owned(),
+        controller2.txns.read().derive_add_ctx(actor),
+    );
+
+    let add_ctx = controller2.history_hashes.read_ctx().derive_add_ctx(actor);
+    let op2 = controller2
+        .history_hashes
+        .update(10u64, add_ctx, |v, a| v.add(vec![8; 20], a));
+
+    let op3 = controller2
+        .candidates
+        .add(vec![8; 20], controller2.txns.read().derive_add_ctx(actor));
+    controller2.apply(Op {
+        dot,
+        txns_op: Some(op1),
+        history_hashes_op: Some(op2),
+        candidates_op: Some(op3),
+    });
+    println!("controller2: {:#?}", controller2);
+
+    controller1.merge(controller2);
+    println!("controller3: {:#?}", controller1);
 }
--- src/proc_macro_one.rs
+++ src/proc_macro_one.rs
@@ -1,8 +1,8 @@
-use crdts::{CmRDT, GCounter, Map, Orswot};
+use crdts::{GCounter, Map, Orswot};
 use crdts_derive::crdt;
 
 #[crdt(u64)]
-#[derive(Default, Debug)]
+#[derive(Default, Debug, Clone)]
 pub struct ControllerOne {
     txns: Orswot<String, String>,
     history_hashes: Map<u64, Orswot<Vec<u8>, u64>, u64>,
@@ -11,7 +11,8 @@ pub struct ControllerOne {
 }
 
 #[test]
-fn test() {
+fn test_cmrdt() {
+    use crdts::CmRDT;
     let mut controller = ControllerOne::default();
     for actor in 1..=100 {
         for counter in 1..=3 {
@@ -43,4 +44,71 @@ fn test() {
     }
     println!("txns num: {:#?}", controller.txns.read().val.len());
     println!("block_height: {:#?}", controller.block_height.read());
+}
+
+#[test]
+fn test_cvrdt() {
+    use crdts::CmRDT;
+    use crdts::CvRDT;
+    use crdts::Dot;
+    let mut controller1 = ControllerOne::default();
+    let mut controller2 = controller1.clone();
+    let actor = 9_742_820;
+    let counter = 1;
+    let dot = Dot::new(actor, counter);
+    let op1 = controller1.txns.add(
+        format!("{actor}-{counter}"),
+        controller1.txns.read().derive_add_ctx(actor.to_string()),
+    );
+
+    let add_ctx = controller1.history_hashes.read_ctx().derive_add_ctx(actor);
+    let op2 = controller1
+        .history_hashes
+        .update(actor, add_ctx, |v, a| v.add(vec![actor as u8; 20], a));
+
+    let op3 = controller1.candidates.add(
+        vec![actor as u8; 20],
+        controller1.candidates.read().derive_add_ctx(actor),
+    );
+
+    let op4 = controller1.block_height.inc(actor);
+    controller1.apply(ControllerOneCrdtOp {
+        dot,
+        txns_op: Some(op1),
+        history_hashes_op: Some(op2),
+        candidates_op: Some(op3),
+        block_height_op: Some(op4),
+    });
+    println!("controller1: {:#?}", controller1);
+
+    let actor = 9_742_821;
+    let counter = 1;
+    let dot = Dot::new(actor, counter);
+    let op1 = controller2.txns.add(
+        format!("{actor}-{counter}"),
+        controller2.txns.read().derive_add_ctx(actor.to_string()),
+    );
+
+    let add_ctx = controller2.history_hashes.read_ctx().derive_add_ctx(actor);
+    let op2 = controller2
+        .history_hashes
+        .update(actor, add_ctx, |v, a| v.add(vec![actor as u8; 20], a));
+
+    let op3 = controller2.candidates.add(
+        vec![actor as u8; 20],
+        controller2.candidates.read().derive_add_ctx(actor),
+    );
+
+    let op4 = controller2.block_height.inc(actor);
+    controller2.apply(ControllerOneCrdtOp {
+        dot,
+        txns_op: Some(op1),
+        history_hashes_op: Some(op2),
+        candidates_op: Some(op3),
+        block_height_op: Some(op4),
+    });
+    println!("controller2: {:#?}", controller2);
+
+    controller1.merge(controller2);
+    println!("controller3: {:#?}", controller1);
 }
