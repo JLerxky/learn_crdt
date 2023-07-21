3ecf0f31aad01a2b67336b5fc0abf08b338d619b
JLer <jlerxky@live.com>
2023-07-21 15:34:53 +0800

    test serialization and data size

--- src/proc_macro_one.rs
+++ src/proc_macro_one.rs
@@ -1,12 +1,16 @@
-use crdts::{GCounter, Map, Orswot};
+use crdts::{GCounter, Orswot};
 use crdts_macro::crdt;
 
 #[crdt(u64)]
 pub struct ControllerOne {
-    txns: Orswot<String, String>,
-    history_hashes: Map<u64, Orswot<Vec<u8>, u64>, u64>,
     candidates: Orswot<Vec<u8>, u64>,
     block_height: GCounter<u64>,
+    pool: Pool,
+}
+
+#[crdt(u64)]
+pub struct Pool {
+    txns: Orswot<String, String>,
 }
 
 #[test]
@@ -16,32 +20,38 @@ fn test_cmrdt() {
     for actor in 1..=100 {
         for counter in 1..=3 {
             let dot = crdts::Dot::new(actor, counter);
-            let op1 = controller.txns.add(
+            let txns_op = controller.pool.txns.add(
                 format!("{actor}-{counter}"),
-                controller.txns.read_ctx().derive_add_ctx(actor.to_string()),
+                controller
+                    .pool
+                    .txns
+                    .read_ctx()
+                    .derive_add_ctx(actor.to_string()),
             );
+            let poor_op = PoolCrdtOp {
+                dot,
+                txns_op: Some(txns_op),
+            };
 
-            let add_ctx = controller.history_hashes.read_ctx().derive_add_ctx(actor);
-            let op2 = controller
-                .history_hashes
-                .update(actor, add_ctx, |v, a| v.add(vec![actor as u8; 20], a));
-
-            let op3 = controller.candidates.add(
+            let candidates_op = controller.candidates.add(
                 vec![actor as u8; 20],
                 controller.candidates.read_ctx().derive_add_ctx(actor),
             );
 
-            let op4 = controller.block_height.inc(actor);
-            controller.apply(ControllerOneCrdtOp {
+            let block_height_op = controller.block_height.inc(actor);
+            let controller_op = ControllerOneCrdtOp {
                 dot,
-                txns_op: Some(op1),
-                history_hashes_op: Some(op2),
-                candidates_op: Some(op3),
-                block_height_op: Some(op4),
-            });
+                candidates_op: Some(candidates_op),
+                block_height_op: Some(block_height_op),
+                pool_op: Some(poor_op),
+            };
+            let controller_op_bytes = bincode::serialize(&controller_op).unwrap();
+            println!("controller_op_bytes len: {}", controller_op_bytes.len());
+            let controller_op = bincode::deserialize(&controller_op_bytes).unwrap();
+            controller.apply(controller_op);
         }
     }
-    println!("txns num: {:#?}", controller.txns.read().val.len());
+    println!("txns num: {:#?}", controller.pool.txns.read().val.len());
     println!("block_height: {:#?}", controller.block_height.read());
 }
 
@@ -52,68 +62,69 @@ fn test_cvrdt() {
     use crdts::Dot;
     let mut controller1 = ControllerOne::default();
     let mut controller2 = controller1.clone();
-    let actor = 9_742_820;
-    let counter = 1;
-    let dot = Dot::new(actor, counter);
-    let op1 = controller1.txns.add(
-        format!("{actor}-{counter}"),
-        controller1
-            .txns
-            .read_ctx()
-            .derive_add_ctx(actor.to_string()),
-    );
+    for actor in 1..=300 {
+        let counter = 1;
+        let dot = Dot::new(actor, counter);
+        let txns_op = controller1.pool.txns.add(
+            format!("{actor}-{counter}"),
+            controller1
+                .pool
+                .txns
+                .read_ctx()
+                .derive_add_ctx(actor.to_string()),
+        );
+        let poor_op = PoolCrdtOp {
+            dot,
+            txns_op: Some(txns_op),
+        };
 
-    let add_ctx = controller1.history_hashes.read_ctx().derive_add_ctx(actor);
-    let op2 = controller1
-        .history_hashes
-        .update(actor, add_ctx, |v, a| v.add(vec![actor as u8; 20], a));
+        let candidates_op = controller1.candidates.add(
+            vec![actor as u8; 20],
+            controller1.candidates.read_ctx().derive_add_ctx(actor),
+        );
 
-    let op3 = controller1.candidates.add(
-        vec![actor as u8; 20],
-        controller1.candidates.read_ctx().derive_add_ctx(actor),
-    );
+        let block_height_op = controller1.block_height.inc(actor);
+        controller1.apply(ControllerOneCrdtOp {
+            dot,
+            candidates_op: Some(candidates_op),
+            block_height_op: Some(block_height_op),
+            pool_op: Some(poor_op),
+        });
+        // println!("controller1: {:#?}", controller1);
 
-    let op4 = controller1.block_height.inc(actor);
-    controller1.apply(ControllerOneCrdtOp {
-        dot,
-        txns_op: Some(op1),
-        history_hashes_op: Some(op2),
-        candidates_op: Some(op3),
-        block_height_op: Some(op4),
-    });
-    println!("controller1: {:#?}", controller1);
+        let dot = Dot::new(actor, counter);
+        let txns_op = controller2.pool.txns.add(
+            format!("{actor}-{counter}"),
+            controller2
+                .pool
+                .txns
+                .read_ctx()
+                .derive_add_ctx(actor.to_string()),
+        );
+        let poor_op = PoolCrdtOp {
+            dot,
+            txns_op: Some(txns_op),
+        };
 
-    let actor = 9_742_821;
-    let counter = 1;
-    let dot = Dot::new(actor, counter);
-    let op1 = controller2.txns.add(
-        format!("{actor}-{counter}"),
-        controller2
-            .txns
-            .read_ctx()
-            .derive_add_ctx(actor.to_string()),
-    );
+        let candidates_op = controller2.candidates.add(
+            vec![actor as u8; 20],
+            controller2.candidates.read_ctx().derive_add_ctx(actor),
+        );
 
-    let add_ctx = controller2.history_hashes.read_ctx().derive_add_ctx(actor);
-    let op2 = controller2
-        .history_hashes
-        .update(actor, add_ctx, |v, a| v.add(vec![actor as u8; 20], a));
+        let block_height_op = controller2.block_height.inc(actor);
+        controller2.apply(ControllerOneCrdtOp {
+            dot,
+            candidates_op: Some(candidates_op),
+            block_height_op: Some(block_height_op),
+            pool_op: Some(poor_op),
+        });
+        // println!("controller2: {:#?}", controller2);
 
-    let op3 = controller2.candidates.add(
-        vec![actor as u8; 20],
-        controller2.candidates.read_ctx().derive_add_ctx(actor),
-    );
+        let controller2 = bincode::serialize(&controller2).unwrap();
+        println!("controller_bytes len: {}", controller2.len());
+        let controller2 = bincode::deserialize(&controller2).unwrap();
 
-    let op4 = controller2.block_height.inc(actor);
-    controller2.apply(ControllerOneCrdtOp {
-        dot,
-        txns_op: Some(op1),
-        history_hashes_op: Some(op2),
-        candidates_op: Some(op3),
-        block_height_op: Some(op4),
-    });
-    println!("controller2: {:#?}", controller2);
-
-    controller1.merge(controller2);
-    println!("controller3: {:#?}", controller1);
+        controller1.merge(controller2);
+    }
+    // println!("controller3: {:#?}", controller1);
 }
